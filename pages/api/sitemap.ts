0bedd1ae512b2b38970809a16f6804f9becddc87
n <reiwa@outlook.com.vn>
2022-12-20 00:15:23 +0900

    update: サイトマップを追加する

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -1,13 +1,48 @@
 import "reflect-metadata"
 import type { NextApiHandler } from "next"
+import db from "db"
 
 const apiHandler: NextApiHandler = async (req, resp) => {
   try {
-    const text = ""
+    const posts = await db.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 2000,
+    })
+
+    const labels = await db.label.findMany({
+      orderBy: { posts: { _count: "desc" } },
+      take: 2000,
+    })
+
+    const users = await db.user.findMany({
+      orderBy: { posts: { _count: "desc" } },
+      take: 2000,
+    })
+
+    const workURLs = posts.map((post) => {
+      return `${process.env.NEXT_PUBLIC_APP_URL}/works/${post.id}`
+    })
+
+    const labelURLs = labels.map((label) => {
+      return `${process.env.NEXT_PUBLIC_APP_URL}/labels/${label.id}`
+    })
+
+    const userURLs = users.map((user) => {
+      return `${process.env.NEXT_PUBLIC_APP_URL}/${user.id}`
+    })
+
+    const text = [
+      process.env.NEXT_PUBLIC_APP_URL,
+      ...workURLs,
+      ...labelURLs,
+      ...userURLs,
+    ].join("\n")
 
     resp.end(text)
   } catch (error) {
-    resp.end()
+    resp.status(500).end()
   }
 }
 
